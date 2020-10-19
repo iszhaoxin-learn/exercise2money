f7a4de3d0829b2ea698fd7c4b988f16aebbd6d96
lcmaster-hx <hanskathx@gmail.com>
2020-10-19 21:58:27 +0900

    add 160

---
+++ Week4/160.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        
+        ListNode tmpA = headA, tmpB = headB;
+        int lenA = 0, lenB = 0;
+        
+        while (tmpA != null) {
+            lenA++;
+            tmpA = tmpA.next;
+        }
+        
+        while (tmpB != null) {
+            lenB++;
+            tmpB = tmpB.next;
+        }
+        
+        tmpA = headA;
+        tmpB = headB;
+        if (lenA > lenB) {
+            for (int i = 0; i < lenA - lenB; i++) tmpA = tmpA.next;
+        } else {
+            for (int i = 0; i < lenB - lenA; i++) tmpB = tmpB.next;
+        }
+        
+        while (tmpA != null) {
+            if (tmpA == tmpB) return tmpA;
+            else {
+                tmpA = tmpA.next;
+                tmpB = tmpB.next;
+            }
+        }
+        
+        return null;
+    }
+}
